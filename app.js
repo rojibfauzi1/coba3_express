1c466cdfb6e83f93d0eb7c11abede5c82c8f114c
rojibfauzi1 <rojibfauzi@gmail.com>
2016-09-20 17:24:06 +0700

    commit first

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var path = require("path");
+/*var router = express.Router();*/
+var bodyparser = require("body-parser");
+
+var app = express();
+//port server
+app.set('port',process.env.PORT || 9999);
+
+//view engine
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+//routing 
+app.get("/",function(req,res){
+	res.render('index',{
+		title : 'apps expressjs',
+		items : [
+			{id:1,name: 'zainn'},
+			{id:2,name: 'rojib'},
+			{id:3,name: 'fauzi'}
+		]
+	}); //merender view engine 
+});
+/*module.exports = router;*/
+
+app.listen(app.get('port'),function(req,res){
+	console.log("Server is running on port "+app.get('port'));
+});
