bf8ac7022d2ef96d2db15676cf7cb6c58bfa7b2a
rojibfauzi1 <rojibfauzi@gmail.com>
2016-09-20 19:11:08 +0700

    add item

--- app.js
+++ app.js
@@ -8,24 +8,36 @@ var app = express();
 app.set('port',process.env.PORT || 9999);
 
 //bodyparser
-app.use(bodyparser());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
 
 //view engine
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
+var todoItems = [
+			{id:1,name: 'zainn'},
+			{id:2,name: 'rojib'},
+			{id:3,name: 'fauzi'}
+		];
+
 //routing 
 app.get("/",function(req,res){
 	res.render('index',{
 		title : 'apps expressjs',
-		items : [
-			{id:1,name: 'zainn'},
-			{id:2,name: 'rojib'},
-			{id:3,name: 'fauzi'}
-		]
+		items : todoItems
 	}); //merender view engine 
 });
-/*module.exports = router;*/
+
+app.post("/add",function(req,res){
+	var newList = req.body.newList;
+
+	todoItems.push({
+		id: todoItems.length+1,
+		name: newList
+	});
+	res.redirect("/");
+});
 
 app.listen(app.get('port'),function(req,res){
 	console.log("Server is running on port "+app.get('port'));
